be86ad1ae9b65bd49cb2924e2213afab71712800
Peter Drake <drake@lclark.edu>
2020-05-09 20:05:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Liz: Let's try an experiment in collaboration
+
+# Here's a function definition:
+def square(x):
+  return x * x
+
+# Here's an invocation:
+print(square(4))
+
+# Run it by clicking the "run" button at the top
+
+# Now can you try defining and invoking "cube" below this?
+
+# If this works, it could be fantastic for interactive live coding.
